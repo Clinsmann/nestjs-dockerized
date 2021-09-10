8d9ffec175aee844d6b3bf575778a9802cd0b74e
hillary Ibeanu <ibeanuhillary@gmail.com>
2021-09-10 08:05:22 +0100

    added user enpoint

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:14-alpine
+
+# WORKDIR /usr/src/app
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN yarn
+
+COPY . .
+
+RUN yarn build
+
+# EXPOSE 8000
+
+# CMD ["node", "dist/main"]
+# the above for starting properly
+
+EXPOSE 8000
+
+# CMD ["node", "dist/main"]
+
+# CMD ["yarn", "start:prod"]
+
+# CMD ["yarn", "start"]
+
+CMD ["yarn", "start:dev"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
+console.log({ ssss: process.env.DATABASE_URL });
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL as string,
+      autoLoadEntities: true,
+      synchronize: true,
+
+      logging: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/**/*.migration{.ts,.js}'],
+      subscribers: ['dist/**/*.subscriber{.ts,.js}'],
+      cli: {
+        entitiesDir: 'src/app/models',
+        migrationsDir: 'src/migration',
+        subscribersDir: 'src/subscriber',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
